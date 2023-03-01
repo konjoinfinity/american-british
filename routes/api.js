a556f09c925bf06097847185a787a6651cce1e02
Wesley Scholl <clients@konjoweb.com>
2023-03-01 13:40:04 -0500

    updated files, first test

--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,33 @@
-'use strict';
-
-const Translator = require('../components/translator.js');
+"use strict";
+const Translator = require("../components/translator.js");
 
 module.exports = function (app) {
-  
   const translator = new Translator();
 
-  app.route('/api/translate')
-    .post((req, res) => {
-      
-    });
+  app.route("/api/translate").post((req, res) => {
+    console.log("req.body :>> ", req.body);
+    const { text, locale } = req.body;
+    if (!locale || text == undefined) {
+      res.json({ error: "Required field(s) missing" });
+      return;
+    }
+    if (text == "") {
+      res.json({ error: "No text to translate" });
+      return;
+    }
+    let translation = "";
+    if (locale == "american-to-british") {
+      translation = translator.toBritishEnglish(text);
+    } else if (locale == "british-to-american") {
+      translation = translator.toAmericanEnglish(text);
+    } else {
+      res.json({ error: "Invalid value for locale field" });
+      return;
+    }
+    if (translation == text || !translation) {
+      res.json({ text, translation: "Everything looks good to me!" });
+    } else {
+      res.json({ text, translation: translation[1] });
+    }
+  });
 };
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,8 +1,17 @@
-const chai = require('chai');
+const chai = require("chai");
 const assert = chai.assert;
 
-const Translator = require('../components/translator.js');
+const Translator = require("../components/translator.js");
+let translator = new Translator();
 
-suite('Unit Tests', () => {
-
+suite("Unit Tests", () => {
+  suite("Translate to British English", function () {
+    test("Translate Mangoes are my favorite fruit. to British English", function (done) {
+      assert.equal(
+        translator.toBritishEnglish("Mangoes are my favorite fruit.")[0],
+        "Mangoes are my favourite fruit."
+      );
+      done();
+    });
+  });
 });
